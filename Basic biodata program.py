2d40d76c5381a7fe2b7f88bf97f8b3fcd0e1d635
Ibad Ahmed <ibadimran15@gmail.com>
2026-06-30 02:37:02 +0500

    Add files via upload

---
+++ Basic biodata program.py
@@ -0,0 +1,10 @@
+#Basic biodata entry using python.
+print("Welcome to the Biodata Entry Program!")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+print("*******************")
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
